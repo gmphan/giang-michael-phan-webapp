2b062f6b30f50a9a824b36c42f9e974b4d6255e6
Giang Phan <GiangPhan@Clayton.edu>
2024-09-17 12:04:19 -0400

    Added QuoteCollection migration.

---
+++ src/Gmphan.ModelLib/QuoteCollection.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gmphan.ModelLib
+{
+    public class QuoteCollection
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string? Quote { get; set; }
+        [Required]
+        public string? Author { get; set; }
+        [Required]
+        public string? Type { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
